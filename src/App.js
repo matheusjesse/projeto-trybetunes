59bdc89989d3c13b84ed7654b6fd283d3acf8674
matheusjesse <matheus_jesse@hotmail.com>
2022-03-07 17:40:04 -0300

    arrumando erros de Lint no appJS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,22 +7,19 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
-import { Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return (      
-        <>
-          <Switch>
-            <Route path="/" component={ Login } exact />
-            <Route path="/search" component={ Search }  />
-            <Route path="/album/:id" component={ Album }  />
-            <Route path="/favorites" component={ Favorites } />
-            <Route path="/profile" component={ Profile } exact/>
-            <Route path="/profile/edit" component={ ProfileEdit } exact/>
-            <Route path="*" component={ NotFound } />
-          </Switch>
-        </>
+    return (
+      <Switch>
+        <Route path="/" component={ Login } exact />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } exact />
+        <Route path="/profile/edit" component={ ProfileEdit } exact />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     );
   }
 }
