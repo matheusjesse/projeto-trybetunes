9d96875e3ee6abda3eeecb565c5dfb58e5b1b930
matheusjesse <matheus_jesse@hotmail.com>
2022-03-07 16:42:06 -0300

    criando componente favorites e adiconando roda

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
+import Favorites from './pages/Favorites';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,8 @@ class App extends React.Component {
         <Route path="/" component={ Login } exact />
         <Route path="/search" component={ Search } exact />
         <Route path="/album/:id" component={ Album } exact />
+        <Route path="/favorites" component={ Favorites } exact />
+
       </>
     );
   }
