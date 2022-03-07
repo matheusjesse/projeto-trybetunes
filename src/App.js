5ea8f97f74160c27fff94a7bc4845935c274efbf
matheusjesse <matheus_jesse@hotmail.com>
2022-03-07 17:18:44 -0300

    requisito 01 C O M P L E T O

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ class App extends React.Component {
             <Route path="/search" component={ Search }  />
             <Route path="/album/:id" component={ Album }  />
             <Route path="/favorites" component={ Favorites } />
-            <Route path="/profile" component={ Profile } />
-            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } exact/>
+            <Route path="/profile/edit" component={ ProfileEdit } exact/>
             <Route path="*" component={ NotFound } />
           </Switch>
         </>
