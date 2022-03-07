8be8bcfa81599b182d03a7528cc0633164f7da0a
matheusjesse <matheus_jesse@hotmail.com>
2022-03-07 16:36:14 -0300

    adicionando rota Search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
     return (
-      <Route path="/" component={ Login } exact />
+      <>
+        <Route path="/" component={ Login } exact />
+        <Route path="/search" component={ Search } exact />
+      </>
     );
   }
 }
