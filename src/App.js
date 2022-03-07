754f5159ef9f9390d353515e6726ff2a4fc78216
matheusjesse <matheus_jesse@hotmail.com>
2022-03-07 16:39:33 -0300

    criando componente album e adiconando rota

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
+import Album from './pages/Album';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <>
         <Route path="/" component={ Login } exact />
         <Route path="/search" component={ Search } exact />
+        <Route path="/album/:id" component={ Album } exact />
       </>
     );
   }
