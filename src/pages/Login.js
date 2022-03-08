2c2bbb4a7c9196de02863a06b3fbbc9681585451
matheusjesse <matheus_jesse@hotmail.com>
2022-03-08 16:07:46 -0300

    arrumando Lint e implementando o State name onChange

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,36 @@
 import React from 'react';
+// import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.state = {
+      name: '',
+    };
+  }
+
+  handleInputChange({ target }) {
+    const { value } = target;
+    this.setState({ name: value });
+  }
+
   render() {
+    const { name } = this.state;
+
     return (
-      <div data-testid="page-login"> a </div>
+      <div data-testid="page-login">
+        <label htmlFor="name">
+          <input
+            type="text"
+            id="name"
+            data-testid="login-name-input"
+            onChange={ this.handleInputChange }
+            value={ name }
+          />
+        </label>
+        <button type="button" data-testid="login-submit-button">Entrar</button>
+      </div>
     );
   }
 }
