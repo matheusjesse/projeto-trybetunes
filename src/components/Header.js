bc160f617d6b77d41b113de6a941874ec97c3332
matheusjesse <matheus_jesse@hotmail.com>
2022-03-20 13:44:39 -0300

    requisito 04

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Load from '../pages/Load';
 import { getUser } from '../services/userAPI';
 
@@ -26,7 +27,13 @@ class Header extends React.Component {
 
         { userData === '' ? <Load />
           : (
-            <p data-testid="header-user-name">{ userData }</p>)}
+            <>
+              <p data-testid="header-user-name">{ userData }</p>
+              <Link data-testid="link-to-search" to="/search">Search</Link>
+              <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+              <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+            </>
+          )}
       </header>
     );
   }
