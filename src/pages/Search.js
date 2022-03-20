d8e2277f3ff5ae0fa810ecb35fd937d2f5be217e
matheusjesse <matheus_jesse@hotmail.com>
2022-03-20 14:22:20 -0300

    requisito 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,44 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonSearch: true,
+    };
+  }
+
+  validateButton = ({ target }) => {
+    const text = target.value;
+    const minNameLength = 2;
+    const validation = text.length >= minNameLength;
+    if (validation) {
+      this.setState({ buttonSearch: false });
+    } else {
+      this.setState({ buttonSearch: true });
+    }
+  }
+
   render() {
+    const { buttonSearch } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search">b</div>
+        <div data-testid="page-search">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.validateButton }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            value="Search"
+            disabled={ buttonSearch }
+          >
+            Search
+          </button>
+        </div>
       </>
     );
   }
