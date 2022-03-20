631b3162f359987ad88a00165711eee0c283323c
matheusjesse <matheus_jesse@hotmail.com>
2022-03-20 13:30:46 -0300

    requisito 03 em progresso

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Load from '../pages/Load';
+import { getUser } from '../services/userAPI';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: '',
+    };
+  }
+
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  loadUser = async () => {
+    const data = await getUser();
+    this.setState({ userData: data.name });
+  }
+
+  render() {
+    const { userData } = this.state;
+    return (
+      <header data-testid="header-component">
+
+        { userData === '' ? <Load />
+          : (
+            <p>{ userData }</p>)}
+      </header>
+    );
+  }
+}
+
+export default Header;
