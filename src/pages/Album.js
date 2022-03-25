155f16d01154398fdd5034ce1e02312b4bad083d
matheusjesse <matheus_jesse@hotmail.com>
2022-03-24 23:31:44 -0300

    requisito 07 23:31

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,15 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      albumSelected: [],
+      artistName: '',
+      albumName: '',
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.getAlbumsMusics(id);
+  }
+
+  getAlbumsMusics = async (id) => {
+    const musicData = await getMusics(id);
+    this.setState({
+      albumSelected: musicData,
+      artistName: musicData[0].artistName,
+      albumName: musicData[0].collectionName,
+    });
+  }
+
   render() {
+    const { albumName, artistName, albumSelected } = this.state;
+    const filterAlbum = albumSelected.filter((item, index) => index > 0);
     return (
       <>
         <Header />
-        <div data-testid="page-album">c</div>
+        <div data-testid="page-album">
+          <h1 data-testid="artist-name">{ artistName }</h1>
+          <h2 data-testid="album-name">{ albumName }</h2>
+          {
+            filterAlbum.map((music) => (
+              <MusicCard
+                key={ music.trackId }
+                musicName={ music.trackName }
+                musicUrl={ music.previewUrl }
+              />))
+          }
+        </div>
       </>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.object,
+}.isRequired;
 
 export default Album;
