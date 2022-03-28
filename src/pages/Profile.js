cd62086e028b5cfbca7a6d6ce2160a8b6de1cd0b
matheusjesse <matheus_jesse@hotmail.com>
2022-03-28 02:59:22 -0300

    requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      load: false,
+      userInfo: {},
+    }
+  }
+
+  async componentDidMount() {
+    const data = await getUser();
+    this.setState({
+      userInfo: data,
+    });
+  }
+
   render() {
+    const { userInfo } = this.state;
+    const { name, email, image, description } = userInfo;
     return (
       <>
         <Header />
-        <div data-testid="page-profile">f</div>
+        <Link to="/profile/edit">Editar perfil</Link>
+        <div data-testid="page-profile">
+          <img src={image} data-testid="profile-image" alt={name} />
+          <h1>{ name }</h1>
+          <h2>{ email }</h2>
+          <p>{ description } </p>
+        </div>
       </>
     );
   }
